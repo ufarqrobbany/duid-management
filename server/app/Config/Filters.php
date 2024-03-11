54825c075999de98ed7c2dabe46218165dce04b4
ufarqrobbany <faruqrobbany@gmail.com>
2024-03-11 18:55:34 +0700

    update

--- server/app/Config/Filters.php
+++ server/app/Config/Filters.php
@@ -24,6 +24,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'cors'     => \App\Filters\Cors::class, 
     ];
 
     /**
@@ -37,6 +38,7 @@ class Filters extends BaseConfig
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
+            'cors',
         ],
         'after' => [
             'toolbar',
